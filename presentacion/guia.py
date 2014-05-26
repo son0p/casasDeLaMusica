3cbaac239bf8a70a0d0074dcb2da28da5b19ca1f
fede2001 <fede2001@hotmail.com>
2014-05-26 06:32:27 -0500

    boceto

---
+++ presentacion/guia.py
@@ -0,0 +1,31 @@
+# Quien soy y qué hago acá
+
+# Tres valores: Pedagogía, expandir alcance, experimentación
+
+# MVM
+- Desición Política que usa recursos.
+- Entrenamiento en convivencia... infraestructura y métodologías.
+
+# Festival MVM
+- Procesos
+
+# Red
+- Venezuela
+- Intrumentación clásica
+
+# Casas de Música
+- Instrumetación urbana y tradicional.
+- 40 años a la ciudad, construir pedagogía.
+- Vocacional
+- Musicalidad
+- Hacer, Crear, Ensamblar, Documentar.
+- Varias formas de acercarse.
+- 4E - 32N tejidos.
+- Infraestructura especializada.
+
+# Casa de Experimentación
+- Experimentación:  Observatorio para ? decidir, empujar límites, automatizar.
+
+
+
+ 
