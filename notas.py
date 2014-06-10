2fd4efa8cf8f4a7ddb18b8d5bf184364c4c763b7
fede2001 <fede2001@hotmail.com>
2014-06-10 14:02:37 -0500

    organizo la info de varias reuniones

---
+++ notas.py
@@ -0,0 +1,97 @@
+and       del       from      not       while    
+as        elif      global    or        with     
+assert    else      if        pass      yield    
+break     except    import    print              
+class     exec      in        raise              
+continue  finally   is        return             
+def       for       lambda    try
+
+#Notas
+
+
+# Filosofia 
+def ?que tipo de montaje podemos tener vivo con los profesores.? ensambles de formacion clases en vivo
+def ?casa de música urbana
+global ?la música hip hop tiene más que ver con procesos políticos como hacemos? urbana
+global lideres defienden su particularidad, fer caimaniza
+global punto en común pueden ser las herramientas pedagógicas
+global  hay casa de m[usica electrónica +  casa de experimentación, Opcion formación en música no requerimiento
+global ensamble de formato, ensamble interdiciplinar, gran ensamble
+global director referente, coordinar, eficiente, como dirije el equipo, como se dan clases a niños 
+global cuadro de funciones y procesos:  
+if secuenciador analogo parecido al orff
+def ¿que es una filosofía que se convierte en peagogía y luego en classes
+se sumo la pregunta ¿Cual es el rol de profesor? posición circular no dirije el profesor.
+todo es una experiencia viva una negra no es algo estático.
+todas las casas formación de formadores
+todas las casas grupo de proyección Chisga
+director invitado una vez al año
+loso movimientos circulares son mas naturales que los lineales
+
+
+# Pedagogia 
+def al niño, mantenerlo en su mundo no ponerlo a elegir!! él conserva su mundo interior.  esta todo encaminado para que se parezca a el mismo.
+no es quieres ir a mercar .. sino vamos al mercado..
+no compararlo con otros niños
+no esa permitido igualar
+restringen lo que vuelva euf{orico a la masa
+ud no es parte de una masa .. es un individo.
+
+ def los instrumentos son todos .. hay que ensamblar .. educación tipo laboratorio.. complementado con proceso de formación.
+ exec !!!!!! diseñar repertorios que nos permitan llegar a las escuelas y ponerlas en practica (ensamble) volverse un curriculo
+/pedag in Curriculo estático - escuela de música
+try hacer, síntesis gráfica(la que sea), garabateo, 
+if cambiamos metrónomo por techno
+exec Contruir o desde el experimento lo que puede ser herramientas pedagógicas
+ciclo! => 
+ciclo! => desplazados de donde vienen que traen como afectan la m[usica en med .. data-> visualizaci[on
+if tres niveles de projectos
+
+global lo que nos tiene que marcar en las escuelsa es la experiencia, la vivencia, la conexión!!! primero vivelo, experimentalo, y luego vas a aprendiendo
+global recobrar las maneras en que se aprende la música callejera, campesina e traerlo en integrarlo 
+import minima unidad que lleva el profesor para explicar sus conceptos
+if educar urbano en flashmob entregar fanzine educativo
+if usamos las canecas de tableros?
+global nuestro enfoque pedagógico se enfoca en exaltar la emoción y la vivencia.
+global que nota lo que me contaron y vea yo lo estoy haciendo ya.
+def OJO integrar como técnica las señas para el ensamble repentino
+exec como hacer  que las electrónicas ayuden a iniciar otras casas, orff, dani pedro
+la musica electrónica parecido al orff pulso, cuadratura
+Tomados de La conversación de noam chomski con ...
+  Dewey progresive school
+  Descartes te da las herramientas para dudar
+  Estudiantes y profesores deben ser enseñados a cuestionar y retar
+
+# Operatividad
+/op global ajustar roles para ser eficientes : no titrar a josetobon a dar flauta 1
+/op if la usm nos ayuda a identificar ensambles nacientes para que jose tobón aporte
+/filosofia La Red no pueden estar de itaguí .. sacan a los grandes
+/op exec Repertorios para 1 para 2 para  4
+/op def ?Asociacion con la usm Agremiación - Legalidad sayco .. comunicaciones
+exec cuandrar con marta empaque de cosas (canecas)
+def cada linder queda encargado de coordinar sus sets de instrumento
+def enlaces de escuelas (conectores)
+if usamos las canecas de hard case para tambora?
+if cada mes nos reunimos a ver alguna de las dsiciplinas en que van .. vivencial, hubo una realidad sonora
+exec cada uno escoge una sede temporal para experimntar lo que sería su casa-
+
+# Conectores
+/conect exec averiguar laso que fueron a la conferecia de electrönica.
+exec la bola .. y pajaro de monte las propones claudia para aprender
+
+
+# Comunicacion
+try en ensamble medellin vive la música + semilleros gran gente!
+def compartir lo mismo con presentacion diferente
+def estrategia comunicacion talleres
+ecard jpg
+integrar la inscripción por facebook
+
+# Desarrollos
+exec como replicar en masa el secuenciador?
+exec taller de literatura
+
+# Sostenibilidad
+¿las casas se sostienen independientemente? un sistema central
+
+# POOL 
